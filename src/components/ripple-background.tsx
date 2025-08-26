b6642299675a9f0f882b98592dd553b832eef58d
Caio Barbosa <fire.cpmarketing@gmail.com>
2025-08-26 14:06:02 +0000

    quero que use essas configs:

--- src/components/ripple-background.tsx
+++ src/components/ripple-background.tsx
@@ -10,11 +10,15 @@ export function RippleBackground() {
         enableRainbow={false}
         gridColor="#FE4900"
         rippleIntensity={0}
-        gridSize={2}
-        gridThickness={15}
-        mouseInteraction={false}
-        mouseInteractionRadius={1.2}
-        opacity={0.8}
+        gridSize={30}
+        gridThickness={22}
+        fadeDistance={1.5}
+        vignetteStrength={5}
+        glowIntensity={1}
+        opacity={0.65}
+        gridRotation={0}
+        mouseInteraction={true}
+        mouseInteractionRadius={0.2}
       />
     </div>
   );
