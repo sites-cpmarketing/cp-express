7d8ea83ea2ced1b9a56489fed91e3d939be7e891
Caio Barbosa <fire.cpmarketing@gmail.com>
2025-08-26 11:50:26 +0000

    retire do cabeçalho o logo e o nome

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
-import { Logo } from '@/components/logo';
 import { UserNav } from '@/components/layout/user-nav';
 
 export const metadata: Metadata = {
@@ -19,11 +18,10 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;900&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;900&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased flex flex-col min-h-screen">
-        <header className="flex h-16 items-center justify-between border-b px-4 sm:px-6 lg:px-8">
-          <Logo />
+      <body className="font-body antialiased flex flex-col min-h-screen bg-gray-50 dark:bg-gray-950">
+        <header className="flex h-16 items-center justify-end border-b px-4 sm:px-6 lg:px-8">
           <UserNav />
         </header>
         <main className="flex-1 p-4 sm:p-6 lg:p-8">
